cc6b3868cfa2dc1142aff939a575c6da22fb844e
MathiasChirivi <chirivimathias3@gmail.com>
2023-06-29 18:27:58 +0200

    ese finito

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -10,8 +10,10 @@ class PageController extends Controller
 {
     public function index() {
         $today = date('2023-06-29');
-        $trains = Trains::where('data_partenza', '=', 'today')->get();
-        // $trains = Trains::all(["Compagnia", "Codice_treno", "stazione_partenza", "stazione_arrivo","data_partenza"]);
-        return view('welcome', compact("trains"));
+        $trains = [
+                'trainsToday' => Trains::where('data_partenza', '=', $today)->get(),
+                'allTrains' => Trains::all(["Compagnia", "Codice_treno", "stazione_partenza", "stazione_arrivo","data_partenza","orario_partenza","in_orario","viaggio_cancellato"])
+        ]; 
+        return view('welcome', $trains);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,41 @@
     <div class="row g-4">
         <div class="col">
             <div>
-                @foreach ($trains as $train)
-                    <h2> {{$train->data_partenza}} </h2>
-                @endforeach
+                <h1>Treni in partenza oggi</h1>
+
+                @if ($trainsToday->count() > 0)
+                    <ul>
+                        @foreach ($trainsToday as $train)
+                            <li>Compagnia: {{ $train->Compagnia }}</li>
+                            <li>Codice treno: {{ $train->Codice_treno }}</li>
+                            <li>Stazione di partenza: {{ $train->stazione_partenza }}</li>
+                            <li>Stazione di arrivo: {{ $train->stazione_arrivo }}</li>
+                            <li>Data partenza: {{ $train->data_partenza }}</li>
+                            <li>Orario partenza: {{ $train->orario_partenza }}</li>
+                            <br>
+                        @endforeach
+                    </ul>
+                @else
+                    <p>Nessun treno in partenza oggi.</p>
+                @endif
+
+                <h1>Tutti i treni</h1>
+
+                @if ($allTrains->count() > 0)
+                    <ul class="">
+                        @foreach ($allTrains as $train)
+                            <li>Compagnia: {{ $train->Compagnia }}</li>
+                            <li>Codice treno: {{ $train->Codice_treno }}</li>
+                            <li>Stazione di partenza: {{ $train->stazione_partenza }}</li>
+                            <li>Stazione di arrivo: {{ $train->stazione_arrivo }}</li>
+                            <li>Data partenza: {{ $train->data_partenza }}</li>
+                            <li>Orario partenza: {{ $train->orario_partenza }}</li>
+                            <br>
+                        @endforeach
+                    </ul>
+                @else
+                    <p>Nessun treno disponibile.</p>
+                @endif
             </div>
         </div>
     </div>
